46f9c9b88bf17370c6575198d6c8caa6ce168865
Antony Kellermann <antony@aokellermann.dev>
2024-04-06 18:14:57 -0400

    it's working

--- content/nexus.ts
+++ content/nexus.ts
@@ -1,10 +1,13 @@
-import type { ZoteroItem, IZotero, ZoteroObserver } from '../typings/zotero'
-import { ItemPane } from './itemPane'
-import { ToolsPane } from './toolsPane'
-import { PrefPane } from './prefPane'
-import { ZoteroUtil } from './zoteroUtil'
+import type {ZoteroItem, IZotero, ZoteroObserver, ZoteroResolver, IServices} from '../typings/zotero'
+import {ItemPane} from './itemPane'
+import {ToolsPane} from './toolsPane'
+import {PrefPane} from './prefPane'
+import {ZoteroUtil} from './zoteroUtil'
+
+import jspath from 'jspath'
 
 declare const Zotero: IZotero
+declare const Services: IServices
 declare const window
 
 enum HttpCodes {
@@ -35,6 +38,7 @@ class Nexus {
   public ItemPane: ItemPane
   public PrefPane: PrefPane
   public ToolsPane: ToolsPane
+  private oldPDFResolverFunction
 
   constructor() {
     this.ItemPane = new ItemPane()
@@ -113,6 +117,228 @@ class Nexus {
   }
 
   public load(): void {
+    this.oldPDFResolverFunction = Zotero.Attachments.getPDFResolvers
+
+
+    Zotero.Attachments.getPDFResolvers = (item, methods, automatic) => {
+      if (!methods) {
+        methods = ['doi', 'url', 'oa', 'custom']
+      }
+
+      const useDOI = methods.includes('doi')
+      const useURL = methods.includes('url')
+      const useOA = methods.includes('oa')
+      const useCustom = methods.includes('custom')
+
+      const resolvers: any[]  = []
+      let doi = item.getField('DOI') || item.getExtraField('DOI')
+      doi = Zotero.Utilities.cleanDOI(doi)
+
+      if (useDOI && doi) {
+        doi = Zotero.Utilities.cleanDOI(doi)
+        if (doi) {
+          resolvers.push({
+            pageURL: `https://doi.org/${doi}`,
+            accessMethod: 'doi',
+          } as ZoteroResolver)
+        }
+      }
+
+      if (useURL) {
+        let url = item.getField('url')
+        if (url) {
+          url = Zotero.Utilities.cleanURL(url)
+          if (url) {
+            resolvers.push({
+              pageURL: url,
+              accessMethod: 'url',
+            })
+          }
+        }
+      }
+
+      if (useOA && doi) {
+        resolvers.push(async () => {
+          const urls = await Zotero.Utilities.Internal.getOpenAccessPDFURLs(doi)
+          const xx: ZoteroResolver[] = urls.map(o => ({
+            url: o.url,
+            pageURL: o.pageURL,
+            articleVersion: o.version,
+            accessMethod: 'oa',
+          }))
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+          return xx
+        })
+      }
+
+      if (useCustom && doi) {
+        let customResolvers
+        try {
+          customResolvers = Zotero.Prefs.get('findPDFs.resolvers')
+          if (customResolvers) {
+            customResolvers = JSON.parse(customResolvers)
+          }
+        } catch (e) {
+          Zotero.debug('Error parsing custom PDF resolvers', 2)
+          Zotero.debug(e, 2)
+        }
+
+        if (customResolvers) {
+          // Handle single object instead of array
+          if (!Array.isArray(customResolvers) && customResolvers.method) {
+            customResolvers = [customResolvers]
+          }
+          if (Array.isArray(customResolvers)) {
+            const setting = 'zoteronexus.automatic'
+            if (Zotero.Prefs.get(setting) === undefined) {
+              Zotero.Prefs.set(setting, true)
+            }
+
+            customResolvers.push(
+              {
+                name: 'Nexus',
+                method: 'GET',
+                url: 'https://hub.libstc.cc/{doi}.pdf',
+                mode: 'pdf',
+                selector: '#pdf',
+                attribute: 'src',
+                automatic:  Zotero.Prefs.get(setting) as boolean,
+                timeout: 30000,
+              })
+            // Only include resolvers that have opted into automatic processing
+            if (automatic) {
+              customResolvers = customResolvers.filter(r => r.automatic)
+            }
+
+            for (const resolver of customResolvers) {
+              try {
+                let {
+                  name,
+                  method,
+                  url,
+                  mode,
+                  selector,
+                  timeout,
+
+                  // HTML
+                  attribute,
+                  index,
+
+                  // JSON
+                  mappings,
+                } = resolver
+                if (!name) {
+                  throw new Error("'name' not provided")
+                }
+                if (!['GET', 'POST'].includes(method.toUpperCase())) {
+                  throw new Error("'method' must be 'GET' or 'POST'")
+                }
+                if (!url) {
+                  throw new Error("'url' not provided")
+                }
+                if (!url.includes('{doi}')) {
+                  throw new Error("'url' must include '{doi}'")
+                }
+                if (!['html', 'json', 'pdf'].includes(mode.toLowerCase())) {
+                  throw new Error("'mode' must be 'html' or 'json'")
+                }
+                if (!selector && mode !== 'pdf') {
+                  throw new Error("'selector' not provided")
+                }
+
+                url = url.replace(/\{doi}/, doi)
+
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                // @ts-ignore
+                resolvers.push(async () => {
+                  Zotero.debug(`Looking for PDFs for ${doi} via ${name}`)
+
+                  const req = await Zotero.HTTP.request(
+                    method.toUpperCase(),
+                    url,
+                    {
+                      responseType: mode === 'json' ? 'json' : 'document',
+                      timeout,
+                    }
+                  )
+
+                  if (mode === 'pdf') {
+                    return [{
+                      accessMethod: name,
+                      url,
+                      referrer: url,
+                    }]
+                  } else if (mode === 'html') {
+                    const doc = req.response
+                    const elem = index
+                      ? doc.querySelectorAll(selector).item(index)
+                      : doc.querySelector(selector)
+                    if (!elem) return []
+                    let val = attribute
+                      ? elem.getAttribute(attribute)
+                      : elem.textContent
+                    if (!val) return []
+
+                    // Handle relative paths
+                    val = Services.io.newURI(
+                      val, null, Services.io.newURI(url)
+                    ).spec
+
+                    return [{
+                      accessMethod: name,
+                      url: val,
+                      referrer: url,
+                    }]
+                  } else if (mode === 'json') {
+                    const json = req.response
+                    let results = jspath.apply(selector, json)
+                    // If mappings for 'url' and 'pageURL' are supplied,
+                    // extract properties from each object in the array
+                    if (mappings) {
+                      const mappedResults: any[] = []
+                      for (const result of results) {
+                        if (typeof result != 'object') continue
+                        const mappedResult: any = {}
+                        for (const field in mappings) {
+                          if (!['url', 'pageURL'].includes(field)) continue
+
+                          if (result[mappings[field]]) {
+                            mappedResult[field] = result[mappings[field]]
+                          }
+                        }
+                        mappedResults.push(mappedResult)
+                      }
+                      results = mappedResults
+                    } else {
+                      results = results
+                        .filter(x => typeof x == 'string')
+                        .map(x => ({url: x}))
+                    }
+                    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+                    return results.map(o => Object.assign(
+                      o,
+                      {
+                        accessMethod: name,
+                        referrer: url,
+                      } as ZoteroResolver
+                    ))
+                  }
+                })
+              } catch (e) {
+                Zotero.debug('Error parsing PDF resolver', 2)
+                Zotero.debug(e, 2)
+                Zotero.debug(resolver, 2)
+              }
+            }
+          }
+        }
+      }
+
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+      return resolvers
+    }
+
+    Zotero.debug('hello from nexus!')
     // Register the callback in Zotero as an item observer
     if (this.initialized) return
     this.observerId = Zotero.Notifier.registerObserver(new ItemObserver(), ['item'], 'Nexus')
@@ -120,6 +346,7 @@ class Nexus {
   }
 
   public unload(): void {
+    Zotero.Attachments.getPDFResolvers = this.oldPDFResolverFunction
     if (this.observerId) {
       Zotero.Notifier.unregisterObserver(this.observerId)
     }
@@ -215,4 +442,4 @@ if (typeof window !== 'undefined') {
   }, false)
 }
 
-export { Nexus }
+export {Nexus}
--- typings/zotero.d.ts
+++ typings/zotero.d.ts
@@ -1,75 +1,104 @@
 interface IZoteroPane {
-  canEdit: () => boolean
-  displayCannotEditLibraryMessage: () => void
-  getSelectedCollection: (asID: boolean) => ZoteroCollection | null
-  getSelectedItems: () => [ZoteroItem]
+    canEdit: () => boolean
+    displayCannotEditLibraryMessage: () => void
+    getSelectedCollection: (asID: boolean) => ZoteroCollection | null
+    getSelectedItems: () => [ZoteroItem]
 }
 
 interface ZoteroCollection {
-  getChildItems: (asIDs: boolean, includeDeleted: boolean) => [ZoteroItem]
+    getChildItems: (asIDs: boolean, includeDeleted: boolean) => [ZoteroItem]
 }
 
 interface ZoteroObserver {
-  notify: (event: string, type: string, ids: [number], extraData: Record<string, any>) => Promise<void>
+    notify: (event: string, type: string, ids: [number], extraData: Record<string, any>) => Promise<void>
 }
 
 interface ZoteroItem {
-  id: string
-  libraryID: string
-  getField: (field: string, unformatted?: boolean, includeBaseMapped?: boolean) => string
-  isRegularItem: () => boolean
-  isCollection: () => boolean
+    id: string
+    libraryID: string
+    getField: (field: string, unformatted?: boolean, includeBaseMapped?: boolean) => string
+    isRegularItem: () => boolean
+    isCollection: () => boolean
+    getExtraField: (field: string) => string
+}
+
+interface ZoteroResolver {
+    url?: string,
+    pageURL?: string
+    articleVersion?: string
+    accessMethod?: string
+    version?: string
+    referrer?: string
 }
 
 interface ProgressWindow {
-  changeHeadline: (headline: string, icon?: string, postText?: string) => void
-  addDescription: (body: string) => void
-  startCloseTimer: (millis: number) => void
-  show: () => void
+    changeHeadline: (headline: string, icon?: string, postText?: string) => void
+    addDescription: (body: string) => void
+    startCloseTimer: (millis: number) => void
+    show: () => void
+}
+
+interface URL {
+    spec: string
+}
+
+interface IServices {
+    io: {
+        newURI: (v1: string, v2?: null, v3?: string | URL) => URL
+    }
 }
 
 interface IZotero {
-  Nexus: import('../content/nexus').Nexus
+    Nexus: import('../content/nexus').Nexus
 
-  debug: (msg: string) => void
-  logError: (err: Error | string) => void
-  launchURL: (url: string) => void
+    debug: (msg: string | unknown, code?: number | undefined) => void
+    logError: (err: Error | string) => void
+    launchURL: (url: string) => void
 
-  Notifier: {
-    registerObserver: (observer: ZoteroObserver, types: string[], id: string, priority?: number) => number // any => ZoteroObserver
-    unregisterObserver: (id: number) => void
-  }
+    Notifier: {
+        registerObserver: (observer: ZoteroObserver, types: string[], id: string, priority?: number) => number // any => ZoteroObserver
+        unregisterObserver: (id: number) => void
+    }
 
-  Prefs: {
-    get: (pref: string) => string | number | boolean
-    set: (pref: string, value: string | number | boolean) => any
-  }
+    Prefs: {
+        get: (pref: string) => string | number | boolean
+        set: (pref: string, value: string | number | boolean) => any
+    }
 
-  Items: {
-    getAsync: (ids: number | number[]) => Promise<any | any[]>
-    getAll: () => Promise<ZoteroItem[]>
-  }
+    Items: {
+        getAsync: (ids: number | number[]) => Promise<any | any[]>
+        getAll: () => Promise<ZoteroItem[]>
+    }
 
-  HTTP: {
-    request: (method: string, url: string, options?: {
-      body?: string,
-      responseType?: XMLHttpRequestResponseType,
-      headers?: Record<string, string>,
-      timeout?: number
-    }) => Promise<XMLHttpRequest>
-  }
+    HTTP: {
+        request: (method: string, url: string, options?: {
+            body?: string,
+            responseType?: XMLHttpRequestResponseType,
+            headers?: Record<string, string>,
+            timeout?: number
+        }) => Promise<XMLHttpRequest>
+    }
 
-  Attachments: {
-    importFromURL: (options: Record<string, any>) => Promise<ZoteroItem>
-  }
+    Attachments: {
+        importFromURL: (options: Record<string, any>) => Promise<ZoteroItem>
+        getPDFResolvers: (item: ZoteroItem, methods: string[], automatic: boolean) => any[]
+    }
 
-  Libraries: {
-    isEditable: (libraryId: string) => boolean
-  }
+    Utilities: {
+        cleanDOI: (doi: string) => string
+        cleanURL: (url: string) => string
+        Internal: {
+            getOpenAccessPDFURLs: (doi: string) => Promise<ZoteroResolver[]>
+        }
+    }
 
-  ProgressWindow: {
-    new(): ProgressWindow
-  }
+    Libraries: {
+        isEditable: (libraryId: string) => boolean
+    }
+
+    ProgressWindow: {
+        new(): ProgressWindow
+    }
 }
 
-export { ZoteroItem, ZoteroObserver, IZotero, IZoteroPane, ProgressWindow }
+export {ZoteroItem, ZoteroObserver, IZotero, IZoteroPane, ProgressWindow, ZoteroResolver, IServices}
